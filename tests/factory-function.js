bb81f241959d6906cd2ebd6f83343ebdc57cdf9c
sp010412 <smajwede7@gmail.com>
2021-08-11 09:27:06 +0200

    setup

---
+++ tests/factory-function.js
@@ -0,0 +1,141 @@
+function catelogue() {
+
+    var shoes = [
+        {
+            color: 'green',
+            brand: "Nike",
+            price: 750,
+            in_stock: 10
+        },
+        {
+            color: 'black',
+            brand: "Nike",
+            price: 750,
+            in_stock: 8
+        },
+        {
+            color: 'silver',
+            brand: "Adidas",
+            price: 850,
+            in_stock: 12
+        },
+        {
+            color: 'blue',
+            brand: "Vans",
+            price: 500,
+            in_stock: 20
+        },
+        {
+            color: 'pink',
+            brand: "Vans",
+            price: 450,
+            in_stock: 14
+        },
+        {
+            color: 'red',
+            brand: "Air force",
+            price: 999,
+            in_stock: 7
+        },
+        {
+            color: 'orange',
+            brand: "Puma",
+            price: 650,
+            in_stock: 20
+        },
+        {
+            color: 'light blue',
+            brand: "Nike",
+            price: 1200,
+            in_stock: 10
+        },
+        {
+            color: 'black',
+            brand: "Reebok",
+            price: 1300,
+            in_stock: 23
+        },
+        {
+            color: 'blue',
+            brand: "New balance",
+            price: 950,
+            in_stock: 15
+        },
+        {
+            color: 'white',
+            brand: "Reebok",
+            price: 850,
+            in_stock: 23
+        },
+        {
+            color: 'orange',
+            brand: "Fila",
+            price: 1500,
+            in_stock: 15
+        },
+        {
+            color: 'gold',
+            brand: "fila",
+            price: 1600,
+            in_stock: 9
+        },
+        {
+            color: 'silver',
+            brand: "Supega",
+            price: 750,
+            in_stock: 11
+        },
+        {
+            color: 'blue',
+            brand: "Supega",
+            price: 750,
+            in_stock: 13
+        },
+        {
+            color: 'pink',
+            brand: "Supega",
+            price: 750,
+            in_stock: 5
+        },
+        {
+            color: 'black',
+            brand: "asics",
+            price: 350,
+            in_stock: 5
+        },
+        {
+            color: 'blue',
+            brand: "asics",
+            price: 350,
+            in_stock: 5
+        },
+        {
+            color: 'silver',
+            brand: "asics",
+            price: 350,
+            in_stock: 5
+        },
+        {
+            color: 'brown',
+            brand: "asics",
+            price: 350,
+            in_stock: 5
+        },
+    ]
+
+
+function check() {
+
+}
+
+
+// function () {
+    
+// }
+
+
+
+    return {
+check,
+    }
+}
