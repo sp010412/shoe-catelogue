e8b2cdc94b854a8db45100893cf316cf7e2e727d
sp010412 <smajwede7@gmail.com>
2021-08-11 16:49:14 +0200

    finalised

---
+++ dom.js
@@ -0,0 +1,9 @@
+var findElem = document.querySelector(".find")
+var addToCartElem = document.querySelector(".add")
+var colorDropElem = document.querySelector(".slct1")
+var brandDropElem = document.querySelector(".slct2")
+var sizeDropElem = document.querySelector(".slct3")
+var feedbackElem = document.querySelector(".feedback")
+
+//my instance
+var catalogueInsta = catalogue()
--- tests/factory-function.js
+++ tests/factory-function.js
@@ -1,99 +1,114 @@
-function catelogue() {
-
+function catalogue() {
+    var count = 0
     var shoes = [
         {
             color: 'green',
             brand: "Nike",
             price: 750,
+            size: 5,
             in_stock: 10
         },
         {
             color: 'black',
             brand: "Nike",
             price: 750,
+            size: 5,
             in_stock: 8
         },
         {
             color: 'silver',
             brand: "Adidas",
             price: 850,
+            size: 5,
             in_stock: 12
         },
         {
             color: 'blue',
             brand: "Vans",
             price: 500,
+            size: 5,
             in_stock: 20
         },
         {
             color: 'pink',
             brand: "Vans",
             price: 450,
+            size: 5,
             in_stock: 14
         },
         {
             color: 'red',
             brand: "Air force",
             price: 999,
+            size: 5,
             in_stock: 7
         },
         {
             color: 'orange',
             brand: "Puma",
             price: 650,
+            size: 5,
             in_stock: 20
         },
         {
             color: 'light blue',
             brand: "Nike",
             price: 1200,
+            size: 5,
             in_stock: 10
         },
         {
             color: 'black',
             brand: "Reebok",
             price: 1300,
+            size: 5,
             in_stock: 23
         },
         {
             color: 'blue',
             brand: "New balance",
             price: 950,
+            size: 5,
             in_stock: 15
         },
         {
             color: 'white',
             brand: "Reebok",
             price: 850,
+            size: 5,
             in_stock: 23
         },
         {
             color: 'orange',
             brand: "Fila",
             price: 1500,
+            size: 5,
             in_stock: 15
         },
         {
             color: 'gold',
             brand: "fila",
             price: 1600,
+            size: 5,
             in_stock: 9
         },
         {
             color: 'silver',
-            brand: "Supega",
+            brand: "Superga",
             price: 750,
+            size: 5,
             in_stock: 11
         },
         {
             color: 'blue',
-            brand: "Supega",
+            brand: "Superga",
             price: 750,
+            size: 5,
             in_stock: 13
         },
         {
             color: 'pink',
-            brand: "Supega",
+            brand: "Superga",
             price: 750,
             in_stock: 5
         },
@@ -101,37 +116,37 @@ function catelogue() {
             color: 'black',
             brand: "asics",
             price: 350,
+            size: 5,
             in_stock: 5
         },
         {
             color: 'blue',
             brand: "asics",
             price: 350,
+            size: 5,
             in_stock: 5
         },
         {
             color: 'silver',
             brand: "asics",
             price: 350,
+            size: 5,
             in_stock: 5
         },
         {
             color: 'brown',
             brand: "asics",
             price: 350,
+            size: 5,
             in_stock: 5
         },
     ]
 
 
 function check() {
-
+return shoes;
 }
 
-
-// function () {
-    
-// }
 
 
 
