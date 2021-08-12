3620034d0207cee9a343be7275e436475a4b1f4b
sp010412 <smajwede7@gmail.com>
2021-08-12 10:33:36 +0200

    finalised

--- dom.js
+++ dom.js
@@ -6,20 +6,37 @@ var sizeDropElem = document.querySelector(".slct3")
 var feedbackElem = document.querySelector(".feedback")
 
 //my instance
-var catalogueInsta = catalogue()
+var catalogueInsta = catalogue() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //finding a item on a list 
-function findItems(colorDropElem, brandDropElem) {
-    var shoes = catalogueInsta.shoeList()
-    for (var i = 0; i < shoes.length; i++) {
-        var list = shoes[i];
-        let filteredList = list.filter(function (one, two) {
-            return Number(one.size.includes(colorDropElem.value))
-                && Number(one.size.includes(brandDropElem.value))
-                && Number(two.price.includes(colorDropElem.value))
-                && Number(two.price.includes(brandDropElem.value))
-        });
-        return filteredList;
-    }
-}
-console.log(findItems())
+// function findItems(colorDropElem, brandDropElem) {
+//     var shoes = catalogueInsta.shoeList()
+//     for (var i = 0; i < shoes.length; i++) {
+//         var list = shoes[i];
+//         let filteredList = list.filter(function (one, two) {
+//             return Number(one.size.includes(colorDropElem.value))
+//                 && Number(one.size.includes(brandDropElem.value))
+//                 && Number(two.price.includes(colorDropElem.value))
+//                 && Number(two.price.includes(brandDropElem.value))
+//         });
+//         return filteredList;
+//     }
+// }
+// console.log(findItems())
--- tests/factory-function.js
+++ tests/factory-function.js
@@ -1,165 +1,160 @@
 function catalogue() {
-    var count = 0
-    var shoes = [
-        {
-            color: 'green',
-            brand: "Nike",
-            price: 750,
-            size: 5,
-            in_stock: 10
+    // var count = 0
+    var shoes = {
+        "1": {
+            "color": "green",
+            "brand": "Nike",
+            "price": 750,
+            "size": 5,
+            "in_stock": 10
         },
-        {
-            color: 'black',
-            brand: "Nike",
-            price: 750,
-            size: 5,
-            in_stock: 8
+        "2": {
+            "color": "black",
+            "brand": "Nike",
+            "price": 750,
+            "size": 5,
+            "in_stock": 8
         },
-        {
-            color: 'silver',
-            brand: "Adidas",
-            price: 850,
-            size: 5,
-            in_stock: 12
+        "3": {
+            "color": "silver",
+            "brand": "Adidas",
+            "price": 850,
+            "size": 5,
+            "in_stock": 12
         },
-        {
-            color: 'blue',
-            brand: "Vans",
-            price: 500,
-            size: 5,
-            in_stock: 20
+        "4": {
+            "color": "blue",
+            "brand": "Vans",
+            "price": 500,
+            "size": 5,
+            "in_stock": 20
         },
-        {
-            color: 'pink',
-            brand: "Vans",
-            price: 450,
-            size: 5,
-            in_stock: 14
+        "5": {
+            "color": "pink",
+            "brand": "Vans",
+            "price": 450,
+            "size": 5,
+            "in_stock": 14
         },
-        {
-            color: 'red',
-            brand: "Air force",
-            price: 999,
-            size: 5,
-            in_stock: 7
+        "6": {
+            "color": "red",
+            "brand": "Air force",
+            "price": 999,
+            "size": 5,
+            "in_stock": 7
         },
-        {
-            color: 'orange',
-            brand: "Puma",
-            price: 650,
-            size: 5,
-            in_stock: 20
+        "7": {
+            "color": "orange",
+            "brand": "Puma",
+            "price": 650,
+            "size": 5,
+            "in_stock": 20
         },
-        {
-            color: 'light blue',
-            brand: "Nike",
-            price: 1200,
-            size: 5,
-            in_stock: 10
+        "8": {
+            "color": "light blue",
+            "brand": "Nike",
+            "price": 1200,
+            "size": 5,
+            "in_stock": 10
         },
-        {
-            color: 'black',
-            brand: "Reebok",
-            price: 1300,
-            size: 5,
-            in_stock: 23
+        "9": {
+            "color": "black",
+            "brand": "Reebok",
+            "price": 1300,
+            "": 5,
+            "in_stock": 23
         },
-        {
-            color: 'blue',
-            brand: "New balance",
-            price: 950,
-            size: 5,
-            in_stock: 15
+        "10": {
+            "color": "blue",
+            "brand": "New balance",
+            "price": 950,
+            "size": 5,
+            "in_stock": 15
         },
-        {
-            color: 'white',
-            brand: "Reebok",
-            price: 850,
-            size: 5,
-            in_stock: 23
+        "11": {
+            "color": "white",
+            "brand": "Reebok",
+            "price": 850,
+            "size": 5,
+            "in_stock": 23
         },
-        {
-            color: 'orange',
-            brand: "Fila",
-            price: 1500,
-            size: 5,
-            in_stock: 15
+        "12": {
+            "color": "orange",
+            "brand": "Fila",
+            "price": 1500,
+            "size": 5,
+            "in_stock": 15
         },
-        {
-            color: 'gold',
-            brand: "fila",
-            price: 1600,
-            size: 5,
-            in_stock: 9
+        "13": {
+            "color": "gold",
+            "brand": "fila",
+            "price": 1600,
+            "size": 5,
+            "in_stock": 9
         },
-        {
-            color: 'silver',
-            brand: "Superga",
-            price: 750,
-            size: 5,
-            in_stock: 11
+        "14": {
+            "color": "silver",
+            "brand": "Superga",
+            "price": 750,
+            "size": 5,
+            "in_stock": 11
         },
-        {
-            color: 'blue',
-            brand: "Superga",
-            price: 750,
-            size: 5,
-            in_stock: 13
+        "15": {
+            "color": "blue",
+            "brand": "Superga",
+            "price": 750,
+            "size": 5,
+            "in_stock": 13
         },
-        {
-            color: 'pink',
-            brand: "Superga",
-            price: 750,
-            in_stock: 5
+        "16": {
+            "color": "pink",
+            "brand": "Superga",
+            "price": 750,
+            "in_stock": 5
         },
-        {
-            color: 'black',
-            brand: "asics",
-            price: 350,
-            size: 5,
-            in_stock: 5
+        "17": {
+            "color": "black",
+            "brand": "asics",
+            "price": 350,
+            "size": 5,
+            "in_stock": 5
         },
-        {
-            color: 'blue',
-            brand: "asics",
-            price: 350,
-            size: 5,
-            in_stock: 5
+        "18": {
+            "color": "blue",
+            "brand": "asics",
+            "price": 350,
+            "size": 5,
+            "in_stock": 5
         },
-        {
-            color: 'silver',
-            brand: "asics",
-            price: 350,
-            size: 5,
-            in_stock: 5
-        },
-        {
-            color: 'brown',
-            brand: "asics",
-            price: 350,
+        "19": {
+            "color": 'silver',
+            "brand": "asics",
+            "price": 350,
             size: 5,
             in_stock: 5
         },
-    ]
+        "20": {
+            "color": "brown",
+            "brand": "asics",
+            "price": 350,
+            "size": 5,
+            "in_stock": 5
+        },
+    }
 
 
     function shoeList() {
         return shoes;
     }
 
-    // //finding a item on a list 
-    // function findItems(outputOne, outputTwo) {
-    //     // for (var i = 0; i < shoes.length; i++) {
-    //     var list = shoes;
-    //     let filteredList = list.filter(function (one, two) {
-    //         return Number(one.size.includes(outputOne))
-    //             && Number(one.size.includes(outputTwo))
-    //             && Number(two.price.includes(outputOne))
-    //             && Number(two.price.includes(outputThree))
-    //         // });
-    //     })
-    //     return filteredList;
-    // }
+    //finding a item on a list 
+    function findItems() {
+        console.log(Object.values(shoes).filter(criteria => criteria.color === 'green' 
+        && criteria.brand === 'Nike' 
+        && criteria.size === 5));
+    }
+    //put parameteres
+
 
 
 
@@ -170,3 +165,15 @@ function catalogue() {
         findItems,
     }
 }
+
+
+ //     // for (var i = 0; i < shoes.length; i++) {
+    //     var list = shoes;
+    //     let filteredList = list.filter(function (one, two) {
+    //         return Number(one.size.includes(outputOne))
+    //             && Number(one.size.includes(outputTwo))
+    //             && Number(two.price.includes(outputOne))
+    //             && Number(two.price.includes(outputThree))
+    //         // });
+    //     })
+    //     return filteredList;
